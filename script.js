76ab4c9599351082aa8d42a6c4073229d89e6f32
Rachel Bryant <168672741+rachel-bryant@users.noreply.github.com>
2026-08-14 21:21:28 +0000

    nav bar adjusted for mobile devices

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+// NAVIGATION BAR
+const people_link = document.getElementById("people-link");
+const people_dropdown_content = document.getElementById("people-dropdown-content");
+let people_dropdown_opened = false;
 
+people_link.addEventListener('click', () => {
+    if (!people_dropdown_opened) {
+        people_dropdown_content.style.display = "block";
+        people_dropdown_opened = true;
+    } else {
+        people_dropdown_content.style.display = "none";
+        people_dropdown_opened = false;
+    }
+});
